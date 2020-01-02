e6d6d72ad471348d2119f1f6d70dec1d16fd47d0
olanetsoft <heedris2olubisi@gmail.com>
2020-01-02 12:24:00 +0100

    broken homepage into smaller component and also finish styling

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -28,7 +28,7 @@ constructor(){
               linkUrl: 'shop/sneakers'
             },
             {
-              title: 'womens',
+              title: 'women',
               imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
               size: 'large',
               id: 4,
