25c8dea4092d303bf17a818817841040ce9b8ae6
olanetsoft <heedris2olubisi@gmail.com>
2020-01-02 18:09:25 +0100

    shop category and preview created

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = ()=>(
-  <div>
-    Hats Page loaded
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
